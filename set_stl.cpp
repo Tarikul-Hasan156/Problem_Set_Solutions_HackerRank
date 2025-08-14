14c2f09cadc5e5ff1e46d32570a3d7d9a326cd02
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-14 22:56:51 +0600

    Add files via upload

---
+++ set_stl.cpp
@@ -0,0 +1,61 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+bool check (set<int>&s,int x)
+{
+    return s.find(x)!=s.end();
+}
+void solve ()
+{
+    int q;cin>>q;
+     set<int>s;
+    while (q--)
+    {
+                int y,x;cin>>y>>x;
+           
+            if (y==1)
+            {
+                s.insert(x);
+            }else if (y==2  &&  check (s,x))
+            {
+                
+                s.erase(x);
+            }else if (y==3)
+            {
+                if ( check(s,x))cout<<"Yes"<<endl;
+                else cout<<"No"<<endl;
+            }
+    }
+    
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
