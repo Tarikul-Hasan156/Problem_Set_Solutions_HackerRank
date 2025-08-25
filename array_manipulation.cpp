68adf6f5273188933e498536d8bd343d24b5f0bb
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-25 17:48:44 +0600

    Add files via upload

---
+++ array_manipulation.cpp
@@ -0,0 +1,52 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const  ll N=1e7+15;
+using namespace std;
+void solve ()
+{
+    ll x, q;cin >> x >> q;
+    vector<ll>vv(N,0);
+    while (q--)
+    {
+        ll l,r,val;cin>>l>>r>>val;
+        vv[l]+=val;
+        vv[r+1]-=val;
+    }
+    for (int i=1;i<=N;i++)
+    {
+        vv[i]=vv[i]+vv[i-1];
+    }
+    ll mx=*max_element(vv.begin(),vv.end());
+    cout<<mx<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
