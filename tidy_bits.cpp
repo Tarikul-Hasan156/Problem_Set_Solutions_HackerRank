fef7156cd05c401f7b6634634c330102ca38a0b8
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-23 18:19:52 +0600

    Add files via upload

---
+++ tidy_bits.cpp
@@ -0,0 +1,28 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+     ll x;cin>>x;
+     ll cnt=__builtin_popcountll(x);
+     for (int i=0;i<=x;i++){
+        ll temp=__builtin_popcountll(i);
+        if (temp==cnt){cout<<i<<endl;return;}
+     }
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
