d210359fa971249bcc6d92f2d11bea2acb85b5ba
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-03 18:14:49 +0600

    Add files via upload

---
+++ sum_square_difference.cpp
@@ -0,0 +1,27 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x;cin>>x;
+    ll sum_of_n_num=x*(x+1)/2;
+    sum_of_n_num*=sum_of_n_num;
+    ll square_num=(x*(x+1)*((2*x)+1))/6;
+    cout<<sum_of_n_num-square_num<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
